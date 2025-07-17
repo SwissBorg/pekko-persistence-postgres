13138fe37065680eed14e7bbf1b64c5727551f7b
Scala Steward <scala_steward@virtuslab.com>
2025-07-17 19:51:17 +0000

    Update pekko-persistence-query, ... to 1.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   object Libraries {
     object Pekko {
-      private val Version = "1.1.3"
+      private val Version = "1.1.5"
 
       def slf4j = "org.apache.pekko" %% "pekko-slf4j" % Version
       def persistence = "org.apache.pekko" %% "pekko-persistence-query" % Version
